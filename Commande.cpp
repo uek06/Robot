e8e504f71aad0fbfda2115c0c45d4c06bbc4e09b
Ugo El Koubi <ugo.elkoubi@wanadoo.fr>
2015-12-01 11:02:42 +0100

    commande h et cpp

--- Commande.cpp
+++ Commande.cpp
@@ -1,4 +1,22 @@
-#include "..\..\Documents\VPProjects\Commande.h"
+#include "Commande.h"
+
+map<string, Commande*>& Commande::commandes(){
+	static map<string, Commande*>* buffMap = new map<string, Commande*>;
+  	return *buffMap;
+}
+
+stack<Commande*>& Commande::pile(){
+	static stack<Commande*>* buffStack = new stack<Commande*>;
+  	return *buffStack;
+}
+
+Commande * Commande::newCommande(string s, Robot* r, LecteurCommande* lc){
+	return commandes()[s]->constructeurVirtuel(r,lc);
+}
+
+Commande * Commande::constructeurVirtuel(Robot* r, LecteurCommande* lc){
+	throw ErreurCommandeException();
+}
 
 void Commande::execute() {
 	// TODO - implement Commande::execute
