2f17c0636c2d14e5f83c58f649ba62fbeed9af56
guidezy <guidezyann@gmail.com>
2015-12-01 11:02:29 +0100

    lecteurCommande

--- LecteurCommande.cpp
+++ LecteurCommande.cpp
@@ -3,8 +3,41 @@
 
 static vector<string> liste {"FIGER","REPARTIR","AVANCER","TOURNER","RENCONTRERPLOT","EVALUERPLOT","SAISIR","PESER","POSER"};
 
-void LecteurCommande::readCommande() {
+bool isCommande(string cmd){
+	if(find(liste.begin(),liste.end(), cmd) != liste.end())
+		return true;
+	return false;
+}
+
+void LecteurCommande::readCommand(Robot *r) {
 	cout << "------SIMULATION------" << endl;
-	for(iterator cmd;)
+	for(iterator cmd = liste.begin(); cmd != liste.end(); cmd++){
+		cout << cmd << "|";
+	}
+	cout << endl;
+
+	while(true){
+		string commande;
+		cout << "Entrez une commande:" << endl;
+		cin >> commande;
+		transform(commande.begin(),commande.end(),commande.begin(),::toupper());
+		if(!isCommande(commande)){
+			cout << "Cette commande n'existe pas." << endl;
+		}
+		else if(commande == "ANNULER"){
+
+		}
+		else{
+			try{
+				Commande *c = Commande::nouvelleCommande(commande, r, this);
+				c->execute();
+			}catch(EtatRobot::Wrong_State){
+				cout << "Action impossible" << endl;
+				Commande::pile().pop();
+			}
+		}
+		cin.clear();
+	}
+
 }
 
